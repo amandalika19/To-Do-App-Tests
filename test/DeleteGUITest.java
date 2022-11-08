7c8f5a90a1677b390e56f2fc5c812c6b3518c6af
Annapurna Mandalika <amandalika19@outlook.com>
2022-11-08 15:48:59 -0800

    Updated tests for CliMenuTest and DeleteGUITest.

--- test/DeleteGUITest.java
+++ test/DeleteGUITest.java
@@ -1,15 +1,38 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.TimeZone;
 
 public class DeleteGUITest
 {
+    DeleteGUI newDeleteGUI;
+    static LocalDateTime time;
+    Todo existingTodo;
+
+
+    public DeleteGUITest() {
+        CLIMenu menu = new CLIMenu();
+        time = LocalDateTime.now();
+        existingTodo = new Todo("testTodo", time, Category.Blue, Importance.Low, Status.Partial);
+        newDeleteGUI = new DeleteGUI(existingTodo);
+    }
+
     @Test
-    void deleteGUI_cancelButtonPressed(){
-        Assertions.assertEquals(1, 1);
+    public void DeleteGUI_DeleteConfirmationUICheck() {
+        Assertions.assertTrue(newDeleteGUI.deleteButton.isShowing());
+        Assertions.assertTrue(newDeleteGUI.cancelButton.isShowing());
+    }
+
+    @Test
+    public void DeleteGUI_DeleteButtonClick() {
+        Assertions.assertDoesNotThrow(()-> {
+            newDeleteGUI.deleteButton.doClick();
+        });
+    }
+
+    @Test
+    public void DeleteGUI_CancelButtonClick() {
+        Assertions.assertDoesNotThrow(() -> {
+            newDeleteGUI.cancelButton.doClick();
+        });
     }
 }
